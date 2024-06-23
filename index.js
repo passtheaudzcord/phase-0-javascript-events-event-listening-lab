ea3b57a4acd686ede8d8e467ac05d7fa760ff00c
Audrey <chipmanaudrey@gmail.com>
2024-06-23 08:18:43 -0700

    Use event listeners

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 function addingEventListener() {
+  const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
 }
+
+
+//const input = document.getElementById('button');
+
+//const input = document.getElementById('button');
+
+//function addingEventListener() {
+  //alert('I was clicked!');
+  //input.addEventListener('click', clickAlert);
+//}
+
+//input.addEventListener('click', clickAlert);
